3ac2add82c27dda100be4d7eb58716540f236212
Pavel I <pavel@visualcomposer.com>
2019-11-04 12:23:42 +0200

    VC-603 fix webpack build, react-create-class dependency v2.4.0

---
+++ vc-webpack-vendors/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class TestVcWebpack extends React.Component {
+    render () {
+        return <div>
+            <span>Hello</span>
+        </div>
+    }
+}
