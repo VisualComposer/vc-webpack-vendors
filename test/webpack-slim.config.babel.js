d7f60587e42cac471f2853e9812f56b1e01d15c4
Janis Konutis <mcjohn@inbox.lv>
2022-09-19 11:03:03 +0300

    VC-2649 update webpack config

--- test/webpack-slim.config.babel.js
+++ test/webpack-slim.config.babel.js
@@ -185,6 +185,7 @@ export default Object.assign(
       extensions: ['.ts', '.tsx', '.js', '.json', '.wasm'],
       alias: { public: path.resolve(__dirname, '../builder/public/') },
       fallback: {
+        url: false,
         crypto: require.resolve('crypto-browserify'),
         path: require.resolve('path-browserify'),
         os: require.resolve('os-browserify/browser'),
