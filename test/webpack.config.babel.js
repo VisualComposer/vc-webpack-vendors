e0ced6cdaf2fd7e682f49d6cf4b65e028078190d
Janis Konutis <mcjohn@inbox.lv>
2022-09-19 10:53:06 +0300

    VC-2649 update cypress

--- test/webpack.config.babel.js
+++ test/webpack.config.babel.js
@@ -211,6 +211,7 @@ export default {
     extensions: ['.ts', '.tsx', '.js', '.json', '.wasm'],
     alias: { public: path.resolve(__dirname, '../builder/public/') },
     fallback: {
+      url: false,
       amd: false,
       crypto: require.resolve('crypto-browserify'),
       path: require.resolve('path-browserify'),
